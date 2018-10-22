a5c491a7d60f0f3f569e771889577db748d221e1
Aseem Sharma <er.sharmaaseem@gmail.com>
2018-10-22 11:39:16 +0530

    Updated API key

--- PlaceFinderApp/app/src/main/java/geekboy/placefinder/utils/AppConstants.kt
+++ PlaceFinderApp/app/src/main/java/geekboy/placefinder/utils/AppConstants.kt
@@ -1,4 +1,7 @@
 package geekboy.placefinder.utils
 
 val PREF_NAME = "places_pref"
-const val API_KEY = "AIzaSyBkqdsDAHZhRk3m_c4ooZw8J7wGeSw8oqU"
+// use these key when result are not obtained :
+// 1. AIzaSyBBl8OwbQ84Er83SfdNNIrfFa-cwWCa6Ok
+// 2. AIzaSyBkqdsDAHZhRk3m_c4ooZw8J7wGeSw8oqU
+const val API_KEY = "AIzaSyBBl8OwbQ84Er83SfdNNIrfFa-cwWCa6Ok"
